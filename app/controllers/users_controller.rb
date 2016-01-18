cf238047038158a55846a5cbd06c96c80b01d87d
takeuchihidekijp <takeuchihidekijp@gmail.com>
2016-01-18 21:17:18 +0000

    kadai2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    params.require(:user).permit(:name,:email,:about,:password,:password_confirmation)
   end
   
     def authenticate_user
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160113134642) do
+ActiveRecord::Schema.define(version: 20160118205009) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160113134642) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "about"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
