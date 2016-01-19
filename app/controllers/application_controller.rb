7f92332ea4aa66cd4755a43ccbb52d8592c0d6ac
takeuchihidekijp <takeuchihidekijp@gmail.com>
2016-01-19 12:44:55 +0000

    kadai3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,9 @@ class ApplicationController < ActionController::Base
   private
   def logged_in_user
     unless logged_in?
-    store_location
-    flash[:danger] = "please log in"
-    redirect_to login_url
+      store_location
+      flash[:danger] = "please log in"
+      redirect_to login_url
     end
   end
 end
