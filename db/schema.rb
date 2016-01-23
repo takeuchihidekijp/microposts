bd3f465f343a321be908b9867cec3a8930e810b3
takeuchihidekijp <takeuchihidekijp@gmail.com>
2016-01-23 07:29:51 +0000

    add followings/followers

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20160119135605) do
+ActiveRecord::Schema.define(version: 20160122223721) do
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
@@ -23,9 +22,17 @@ ActiveRecord::Schema.define(version: 20160119135605) do
 
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
   add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
-=======
-ActiveRecord::Schema.define(version: 20160118205009) do
->>>>>>> user-profile
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
